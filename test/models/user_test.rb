2b004cfac01ee6b472a9d99b48caa08867ff8444
ihsannk <ihsannorkhair@gmail.com>
2015-09-17 16:47:55 +0800

    finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,11 +21,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email addresses should be unique" do
-    duplicate_user = @user.dup
-    @user.save
-    assert_not duplicate_user.valid?
-  end
+  # test "email addresses should be unique" do
+  #   duplicate_user = @user.dup
+  #   @user.save
+  #   assert_not duplicate_user.valid?
+  # end
   
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
@@ -35,6 +35,13 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
 end
